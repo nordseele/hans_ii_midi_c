13abf92ac59fbca456a33a668a63e75f31ca750e
Matthias Budde <matt@nordseele.com>
2021-01-13 18:05:40 +0000

    ...

--- update.sh
+++ update.sh
@@ -6,10 +6,10 @@ sudo systemctl stop pigpiod
 sudo systemctl disable pigpiod
 cd
 git clone https://github.com/nordseele/hans_ii_midi_c.git
-cd hans_ii_midi_c
+cd /home/pi/hans_ii_midi_c
 git submodule init
 git submodule update
-cd hans_ii_midi_c/build
+cd /home/pi/hans_ii_midi_c/build
 make
 sudo cp /home/pi/hans_ii_midi_c/hans_ii_midi.service /etc/systemd/system/
 sudo systemctl enable hans_ii_midi.service
